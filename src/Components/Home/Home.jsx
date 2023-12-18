9333dfa5fa37e08cd9d7eb0b5659163df56226a3
temu man <talemie@yahoo.com>
2023-12-17 19:49:03 -0600

    extra imported component removed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,6 @@ import Product from "../Product/Product";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { productList } from "../../CommonResources/prodcutList";
 import HomeCarsoul from "./HomeCarsoul/HomeCarsoul";
-import HomeButtomCarsoul from "./HomeCarsoul/HomeButtomCarsoul";
 function Home() {
 	// product images destructured here
 	const [
